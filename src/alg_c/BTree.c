c15aa3383464bc023bec08ceaaebf75e69a72dc5
tenywen <liupeng@nibirutech.com>
2013-10-14 18:25:33 +0800

    update

--- src/alg_c/BTree.c
+++ src/alg_c/BTree.c
@@ -11,44 +11,68 @@
 #include <stdio.h>
 #include "BTree.h"
 
+extern PBTreeNode NIL;
 // 调整红黑属性
-int _fixed(PBTreeHead head,PBTreeNode node) {
-	PBTreeNode z = node;
-	PBTreeNode parent = _BTREE_PARENT(node);
-	PBTreeNode gparent = NULL; 
+int _insert_fixed(PBTreeHead head,PBTreeNode z) {
 	PBTreeNode uncle = NULL;
-	while(parent && _BTREE_COLOR(parent) == RED) {
-		gparent = _BTREE_PARENT(parent);
-		if (!gparent) 
+	while(_BTREE_PARENT(z) && _BTREE_COLOR((_BTREE_PARENT(z))) == RED) {
+		if (!(_BTREE_PARENT(_BTREE_PARENT(z)))) 
 			break;
-		if(parent == _BTREE_LEFT(gparent))
-			uncle = _BTREE_RIGHT(gparent);
+		if(_BTREE_PARENT(z) == _BTREE_LEFT(_BTREE_PARENT(_BTREE_PARENT(z))))
+			uncle = _BTREE_RIGHT(_BTREE_PARENT(_BTREE_PARENT(z)));
 		else 
-			uncle = _BTREE_LEFT(gparent);
+			uncle = _BTREE_LEFT(_BTREE_PARENT(_BTREE_PARENT(z)));
 		// 节点z的叔叔节点为红色，此为情况1:
 		// 将z的父节点和叔叔节点置为黑，z变更为祖父节点。重复while循环
 		if(_BTREE_COLOR(uncle) == RED) {
 			uncle->Color = BLACK;
-			parent->Color = BLACK;
-			gparent->Color = RED;
-			z = gparent;
-			parent = _BTREE_PARENT(z);
+			_BTREE_PARENT(z)->Color = BLACK;
+			_BTREE_PARENT(_BTREE_PARENT(z))->Color = RED;
+			z = _BTREE_PARENT(_BTREE_PARENT(z));
 		} else {
 			// 节点z的叔叔节点为黑色，此为情况2，3:
-			if(z == _BTREE_RIGHT(parent)) {
-				_LEFT_ROTATE(head,parent,z);
+			if(z == _BTREE_RIGHT(_BTREE_PARENT(z))) {
+				// z上升一层
+				z = _BTREE_PARENT(z);	
+				_LEFT_ROTATE(head,z);
 			}
-			if(z == _BTREE_LEFT(gparent))
-				_RIGHT_ROTATE(head,gparent,z);
+			_BTREE_PARENT(z)->Color = BLACK;
+			_BTREE_PARENT(_BTREE_PARENT(z))->Color = RED;
+			if(_BTREE_PARENT(z) == _BTREE_LEFT(_BTREE_PARENT(_BTREE_PARENT(z))))
+				_RIGHT_ROTATE(head,_BTREE_PARENT(_BTREE_PARENT(z)));
 			else 
-				_LEFT_ROTATE(head,gparent,z);
-			z->Color = BLACK;
-			gparent->Color = RED;
+				_LEFT_ROTATE(head,_BTREE_PARENT(_BTREE_PARENT(z)));
 		}
 	}
 	head->Next->Color = BLACK;
 }
 
+// return min node when root is node
+PBTreeNode BTreeMin(PBTreeNode node) {
+	PBTreeNode left = _BTREE_LEFT(node);
+	PBTreeNode min = node;
+	while(left && !_IS_NIL(left)) {
+		min = left;
+		left = _BTREE_LEFT(left);
+	}
+	return min;
+}
+
+// return node succeed
+PBTreeNode BTreeSucceed(PBTreeNode node) {
+	PBTreeNode right = _BTREE_RIGHT(node);
+	PBTreeNode next = node;
+	PBTreeNode parent = _BTREE_PARENT(node);
+	if (right && !_IS_NIL(right)) {
+		return BTreeMin(right);	
+	}
+	while(parent && next == _BTREE_RIGHT(parent)) { 
+		next = parent;
+		parent = _BTREE_PARENT(next);	
+	}
+	return parent;
+}
+/*
 int BTreeNIL(PBTreeNode p) {
 	PBTreeNode left = (PBTreeNode)malloc(sizeof(BTreeNode));
 	PBTreeNode right = (PBTreeNode)malloc(sizeof(BTreeNode));
@@ -62,56 +86,45 @@ int BTreeNIL(PBTreeNode p) {
 	right->Parent = p;
 	return 0; 
 }
-
-PBTreeNode BTreeCreateNode(int value) {
-	PBTreeNode node = (PBTreeNode)malloc(sizeof(BTreeNode));
-	if(!node)
-		return NULL;
-	_INIT_BTREE_NODE(node);
-	node->Value = value;
-	BTreeNIL(node);
-	return node;
-}
+*/
 
 PBTreeHead BTreeNew() {
 	PBTreeHead head = (PBTreeHead)malloc(sizeof(PBTreeHead));
-	_INIT_BTREE_HEAD(head);
+	head->Next = NIL;
 	return head;
 }
 
 int BTreeInsert(PBTreeHead head,PBTreeNode node) {
-	PBTreeNode parent = NULL;
 	PBTreeNode next = head->Next;
-	if(!next) {
-		head->Next = node;
-		return 0;		
-	}
-	while(_IS_NIL(next) == false) {
-		parent = next;
+	PBTreeNode y = NIL;
+	while(next != NIL) {
+		y = next;
 		if(_BTREE_VALUE(next) > _BTREE_VALUE(node))	
 			next = _BTREE_LEFT(next);
-		else 
+		else
 			next = _BTREE_RIGHT(next);
 	}
-	if (parent) {
-		if (_BTREE_VALUE(parent) > _BTREE_VALUE(node))
-			parent->Left = node;
-		else 
-			parent->Right = node;
-		node->Parent = parent;	
-	} 	
-	
+	if(y != NIL) {
+		if(_BTREE_VALUE(y) > _BTREE_VALUE(node))	
+			y->Left = node;
+		else
+			y->Right = node;
+		node->Parent = y;
+	} else {
+		head->Next = node;		
+	}
+	node->Left = NULL;
+	node->Right = NULL;
 	node->Color = RED;
-	_fixed(head,node);
-	free(next);
+	_insert_fixed(head,node);
 	return 0;
 }
 
 int BTreeMidTraverse(PBTreeNode node) {
-	if (!node) 
-		return 0;	
+	if (node == NIL) 
+		return 0;
 	BTreeMidTraverse(node->Left);
-	if (_IS_NIL(node) == false) {
+	if (node != NIL) {
 		printf("color : %d(1 = red,2 = black)\n",node->Color);
 		printf("value : %d\n",node->Value);
 	}
@@ -121,20 +134,25 @@ int BTreeMidTraverse(PBTreeNode node) {
 
 int main() {
 	PBTreeHead head = BTreeNew();
-	PBTreeNode p1 = BTreeCreateNode(222);
+	PBTreeNode p1 = (PBTreeNode)malloc(sizeof(PBTreeNode));
+	p1->Value = 555;
 	BTreeInsert(head,p1);
-	BTreeMidTraverse(head->Next);
-	PBTreeNode p2 = BTreeCreateNode(333);
-	BTreeInsert(head,p2);
 	
-	BTreeMidTraverse(head->Next);
-	PBTreeNode p3 = BTreeCreateNode(444);
+	PBTreeNode p2 = (PBTreeNode)malloc(sizeof(PBTreeNode));
+	p2->Value = 444;
+	BTreeInsert(head,p2);
+
+	PBTreeNode p3 = (PBTreeNode)malloc(sizeof(PBTreeNode));
+	p3->Value = 666;
 	BTreeInsert(head,p3);
 
-	BTreeMidTraverse(head->Next);
-	PBTreeNode p4 = BTreeCreateNode(555);
+	PBTreeNode p4 = (PBTreeNode)malloc(sizeof(PBTreeNode));
+	p4->Value = 333;
 	BTreeInsert(head,p4);
 
-	BTreeMidTraverse(head->Next);
+	PBTreeNode p5 = (PBTreeNode)malloc(sizeof(PBTreeNode));
+	p5->Value = 222;
+	BTreeInsert(head,p5);
+
 	return 0;
 }
--- src/alg_c/BTree.h
+++ src/alg_c/BTree.h
@@ -30,6 +30,8 @@ struct BTreeHead {
 typedef BTreeNode* PBTreeNode;
 typedef BTreeHead* PBTreeHead;
 
+PBTreeNode NIL = NULL;
+
 #define _BTREE_PARENT(x) (x->Parent)
 #define _BTREE_LEFT(x) (x->Left)
 #define _BTREE_RIGHT(x) (x->Right)
@@ -37,10 +39,7 @@ typedef BTreeHead* PBTreeHead;
 #define _BTREE_COLOR(x) (x->Color)
 #define _SET_BTREE_HEAD(head,x) (head->Next = x)
 #define _IS_NIL(x) ((!x->Left && !x->Right)?true:false)
-#define _INIT_BTREE_HEAD(head) do\
-				{\
-					head->Next = NULL;\
-				}while(0)
+
 #define _INIT_BTREE_NODE(x) do\
 				{\
 					x->Left = NULL;\
@@ -49,9 +48,11 @@ typedef BTreeHead* PBTreeHead;
 					x->Value = 0;\
 					x->Parent = NULL;\
 				}while(0)
-// when y is x->right ,you can left rotate
-#define _LEFT_ROTATE(head,x,y) do\
+
+// when x->right exist,you can left rotate
+#define _LEFT_ROTATE(head,x) do\
 			{\
+				PBTreeNode y = _BTREE_RIGHT(x);\
 				x->Right = y->Left;\
 				y->Left->Parent = x;\
 				y->Left = x;\
@@ -64,18 +65,22 @@ typedef BTreeHead* PBTreeHead;
 					head->Next = y;\
 				x->Parent = y;\
 			}while(0)
-// when x is y->left , you can right rotate
-#define _RIGHT_ROTATE(head,y,x)	do\
+
+// when x->left exist, you can right rotate
+#define _RIGHT_ROTATE(head,x)	do\
 			{\
-				y->Left = x->Right;\
-				x->Right->Parent = y;\
-				x->Parent = y->Parent;\
-				if(_BTREE_PARENT(x) && _BTREE_LEFT(_BTREE_PARENT(y)) == y) \
-					y->Parent->Left = x;\
-				else if(_BTREE_PARENT(x) && _BTREE_RIGHT(_BTREE_PARENT(y)) == y)\
-					y->Parent->Right = x;\
+				PBTreeNode y = _BTREE_LEFT(x);\
+				x->Left = y->Right;\
+				y->Right->Parent = x;\
+				y->Right = x;\
+				y->Parent = x->Parent;\
+				if(_BTREE_PARENT(x) && _BTREE_LEFT(_BTREE_PARENT(x)) == x)\
+					x->Parent->Left = y;\
+				else if(_BTREE_PARENT(x) && _BTREE_RIGHT(_BTREE_PARENT(x)) == x)\
+					x->Parent->Right = y;\
 				else\
 					head->Next = y;\
+				x->Parent = y;\
 			}while(0)
 
 extern PBTreeHead BTreeNew();
